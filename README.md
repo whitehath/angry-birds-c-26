461b7b773341340a7835f5dca29108ea2676f8a3
whitehath <soujanya.amudalaplli@gmail.com>
2021-01-19 19:52:35 +0530

    todays learning

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # angryBirdsStage2.5
 Angry Birds stage 2.5 with Class Inheritance and Images
+we learnt what is git and github and we also pushed the updated version to the new repository.
--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-var platform2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -19,7 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-    platform2=new Ground(200,200,70,30);
+    
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -62,5 +61,5 @@ function draw(){
 
     bird.display();
     platform.display();
-    platform2.display();
+   
 }
